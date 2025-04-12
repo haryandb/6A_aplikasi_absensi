1e2ba3610707c33d4d8df13957e6c5d2a8b59920
Haryan Dwi Baharna <haryandb@gmail.com>
2025-04-12 10:04:35 +0700

    pertemuan 3

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PenggunaController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AbsensiController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', [AdminController::class, "index"]); // ubah
+
+Route::get('/admin/pengguna', [PenggunaController::class, "index"]);
+
+Route::get('/admin/mahasiswa', [MahasiswaController::class, "index"]);
+
+Route::get('/admin/absensi', [AbsensiController::class, "index"]);
